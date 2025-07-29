442ccf24c6f0e5a17452dbdf11f7fe5283a1a4c9
Elaine Cubit <contact@ecubit.dev>
2025-07-29 14:01:40 -0400

    add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 mod watcher;
 
 use crate::watcher::Watcher;
-use std::time;
+use std::time::Duration;
+use clap::Parser;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut watcher = Watcher::new()?;
-    watcher.watch(time::Duration::from_millis(1500), 13)?;
-    Ok(())
+#[derive(Parser)]
+#[command(version, about = "A good music status display for polybar", long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 1500, help="The frequency, in milliseconds, that the display window will update")]
+    update_frequency: u64,
+    #[arg(short, long, default_value_t = 15, help="The length of the display window")]
+    banner_size: usize,
 }
 
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let mut watcher = Watcher::new()?;
+    watcher.watch(
+        Duration::from_millis(args.update_frequency),
+        args.banner_size
+    )?;
+    Ok(())
+}
